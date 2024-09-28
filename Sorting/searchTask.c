8fe15cc34f509d22444c64b80b5a5c8e32dd672d
Artem <109523883+ArtemNikit1n@users.noreply.github.com>
2024-09-28 12:14:23 +0300

    added keyboard input

--- Sorting/searchTask.c
+++ Sorting/searchTask.c
@@ -1,4 +1,24 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <assert.h>
 
 void searchTask(void) {
-	return;
+    char strTotalNumbers[1000];
+    char strRequiredNumbers[1000];
+    char* endptrTotalNumbers;
+    char* endptrRequiredNumbers;
+
+    int totalNumbers = -1;
+    int requiredNumbers = -1;
+
+    printf("Enter a total number of numbers less than 1000");
+    scanf("%s", strTotalNumbers);
+    totalNumbers = strtol(strTotalNumbers, endptrTotalNumbers, 10);
+    asert(totalNumbers > 0 && totalNumbers < 1000);
+
+    printf("Enter the number of numbers less than 1000 you want to find:");
+    scanf("%s", strRequiredNumbers);
+    requiredNumbers = strtol(strRequiredNumbers, endptrRequiredNumbers, 10);
+    asert(totalNumbers > 0 && totalNumbers < 1000);
+
 }
