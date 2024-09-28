b7027a95ae20545ac5a9cbae45945eb0db57e0f9
Artem <109523883+ArtemNikit1n@users.noreply.github.com>
2024-09-28 20:33:11 +0300

    fixScanf

--- Sorting/searchTask.c
+++ Sorting/searchTask.c
@@ -1,12 +1,16 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define _CRT_SECURE_NO_WARNINGS
 
 void searchTask(void) {
-    char strTotalNumbers[1000];
-    char strRequiredNumbers[1000];
-    char* endptrTotalNumbers;
-    char* endptrRequiredNumbers;
+    char strTotalNumbers[10] = {'\n'};
+    char strRequiredNumbers[10];
+    char *endptrTotalNumbers = NULL;
+    char *endptrRequiredNumbers = NULL;
 
     int totalNumbers = -1;
     int requiredNumbers = -1;
@@ -14,11 +18,18 @@ void searchTask(void) {
     printf("Enter a total number of numbers less than 1000");
     scanf("%s", strTotalNumbers);
     totalNumbers = strtol(strTotalNumbers, endptrTotalNumbers, 10);
-    asert(totalNumbers > 0 && totalNumbers < 1000);
+    assert(totalNumbers > 0 && totalNumbers < 1000);
 
     printf("Enter the number of numbers less than 1000 you want to find:");
-    scanf("%s", strRequiredNumbers);
+    if (fgets(strRequiredNumbers, sizeof(strRequiredNumbers), stdin) == NULL) {
+        printf("Input error\n");
+        return;
+    }
     requiredNumbers = strtol(strRequiredNumbers, endptrRequiredNumbers, 10);
-    asert(totalNumbers > 0 && totalNumbers < 1000);
+    assert(requiredNumbers > 0 && requiredNumbers < 1000);
 
+    if (*endptrRequiredNumbers != '\0' || *endptrTotalNumbers != '\0') {
+        printf("Input error");
+        return;
+    }
 }
