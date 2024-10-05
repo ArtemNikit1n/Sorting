3da0c4ea3e658ff1915c84593072a38f27f50385
Artem <109523883+ArtemNikit1n@users.noreply.github.com>
2024-10-05 11:03:46 +0300

    add test

--- Sorting/theMostCommonElementTask.c
+++ Sorting/theMostCommonElementTask.c
@@ -6,6 +6,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 int findTheMostCommonElement(int array[], int arrayLength) {
+    smartQSort(array, 0, arrayLength - 1);
     int maximumLineLength = 1;
     int currentLengthOfTheString = 1;
     int theMostCommonElement = -1;
@@ -25,6 +26,11 @@ int findTheMostCommonElement(int array[], int arrayLength) {
     return theMostCommonElement;
 }
 
+bool testFindTheMostCommonElement() {
+    int testArray[15] = { 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5 };
+    return findTheMostCommonElement(testArray, 15) == 5;
+}
+
 bool theMostCommonElementTask(void) {
     char* endptrArrayLength = NULL;
     char strArrayLength[6] = { 0 };
@@ -32,6 +38,12 @@ bool theMostCommonElementTask(void) {
     int array[1000] = { 0 };
     int arrayLength = -1;
     bool errorCode = false;
+
+    if (!testFindTheMostCommonElement()) {
+        printf("Test failed");
+        errorCode = true;
+        return errorCode;
+    }
 
     printf("Enter the array size:\n");
     scanf("%s", strArrayLength);
@@ -50,13 +62,12 @@ bool theMostCommonElementTask(void) {
         printf("%d ", array[i]);
     }
 
-    smartQSort(array, 0, arrayLength - 1);
+    printf("\nThe most common element: %d", findTheMostCommonElement(array, arrayLength));
 
     printf("\nSorted array: ");
     for (int i = 0; i < arrayLength; i++) {
         printf("%d ", array[i]);
     }
 
-    printf("\nThe most common element: %d", findTheMostCommonElement(array, arrayLength));
     return false;
 }
